0fec50c30cb988907783090989c665b3e78364a8
fydxiix <nazarova152419@gmail.com>
2022-08-05 20:52:01 +0600

    design

--- src/pages/BasketPage.jsx
+++ src/pages/BasketPage.jsx
@@ -30,69 +30,73 @@ function BasketPage() {
   }
 
   return (
-    <div className="basket-page">
-      <Container>
-        <h2>SHOPPING CART</h2>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Item Name:</TableCell>
-              <TableCell>Picture:</TableCell>
-              <TableCell>Price:</TableCell>
-              <TableCell>Count:</TableCell>
-              <TableCell>Amount:</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {basketBooks.products.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell>{item.title}</TableCell>
-                <TableCell>
-                  <img width={60} src={item.thumbnail} alt="" />
-                </TableCell>
-                <TableCell>{item.price} $</TableCell>
-                <TableCell>{item.count}</TableCell>
-                <TableCell>{item.subPrice} $</TableCell>
-                <TableCell>
-                  <Delete
-                    className="basket-delete"
-                    onClick={() => deleteBook(item.id)}
-                  ></Delete>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow className="">
-              <TableCell
-                className="total-tablecell"
-                colSpan={4}
-                style={{ fontSize: "16px", fontWeight: "600" }}
-              >
-                Total Amount:
-              </TableCell>
-              <TableCell
-                className="total-tablecell"
-                colSpan={1}
-                style={{
-                  fontWeight: "500",
-                  fontSize: "16px",
-                  color: "gray",
-                }}
-              >
-                {basketBooks.totalPrice}$
-              </TableCell>
-              <TableCell className="total-tablecell" colSpan={5}>
-                {" "}
-                <Link to="/payment" className="total-link">
-                  {" "}
-                  Buy
-                </Link>
-              </TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </Container>
+    <div className="main-wrapper">
+      <div className="basket-page">
+        <Container>
+          <h2>SHOPPING CART</h2>
+          <div className="sub-wrapper">
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Item Name:</TableCell>
+                  <TableCell>Picture:</TableCell>
+                  <TableCell>Price:</TableCell>
+                  <TableCell>Count:</TableCell>
+                  <TableCell>Amount:</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {basketBooks.products.map((item) => (
+                  <TableRow key={item.id}>
+                    <TableCell>{item.title}</TableCell>
+                    <TableCell>
+                      <img width={60} src={item.thumbnail} alt="" />
+                    </TableCell>
+                    <TableCell>{item.price} $</TableCell>
+                    <TableCell>{item.count}</TableCell>
+                    <TableCell>{item.subPrice} $</TableCell>
+                    <TableCell>
+                      <Delete
+                        className="basket-delete"
+                        onClick={() => deleteBook(item.id)}
+                      ></Delete>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              <TableFooter>
+                <TableRow className="">
+                  <TableCell
+                    className="total-tablecell"
+                    colSpan={4}
+                    style={{ fontSize: "16px", fontWeight: "600" }}
+                  >
+                    Total Amount:
+                  </TableCell>
+                  <TableCell
+                    className="total-tablecell"
+                    colSpan={1}
+                    style={{
+                      fontWeight: "500",
+                      fontSize: "16px",
+                      color: "gray",
+                    }}
+                  >
+                    {basketBooks.totalPrice}$
+                  </TableCell>
+                  <TableCell className="total-tablecell" colSpan={5}>
+                    {" "}
+                    <Link to="/payment" className="total-link">
+                      {" "}
+                      Buy
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              </TableFooter>
+            </Table>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 }
