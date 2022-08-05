95d63d6bc8bf11be7599233f7d62871923616746
fydxiix <nazarova152419@gmail.com>
2022-08-05 16:40:20 +0600

    payment page

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "@mui/material";
+import { Button, Container, TableHead, TableRow } from "@mui/material";
 import { Provider } from "../context/Provider";
 
 function PaymentPage() {
@@ -7,6 +7,18 @@ function PaymentPage() {
     <div className="payment-page">
       <Container>
         <h2>Payment proccess</h2>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Payment amount:</TableCell>
+              <TableCell>Name on card:</TableCell>
+              <TableCell>Card number:</TableCell>
+              <TableCell>Expiry date:</TableCell>
+              <TableCell>Security code:</TableCell>
+              <Button>Pay {}</Button>
+            </TableRow>
+          </TableHead>
+        </Table>
       </Container>
     </div>
   );
